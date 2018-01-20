4d7a685eb82647ce1a70e97e8e57aed0760bc2bb
Ronn Ross <ronn.ross@gmail.com>
2018-01-20 08:13:19 -0500

    Copied over all styles needed. Not all are being generated

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-
 function writeToFile(fileName, contentToWrite) {
     fs.writeFile(fileName, contentToWrite, (err) => {
         if (err) throw err;
@@ -14,65 +13,22 @@ function css(template, ...expressions) {
     })
 }
 
+const range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-/*
+const breakpoints = {
+    '': '0px',
+    '-md': '768px',
+    '-lg': '992'
+}
 
-$spacing-points: (
-  0: "0",
-  8: "0.5rem",
-  16: "1rem",
-  32: "2rem",
-  64: "4rem"
-);
-*/
-
-// const grid = `
-//   ${Array(endRange).fill(1).map(i => {
-//     console.log(i)
-//   })}
-// `
-// function generateSpanStyles(startRange, endRange) {
-//   let acc = '';
-//   for (step = startRange; step <= endRange; step++) {
-//     acc += `.span-${step} {
-//       grid-column-end: span ${step};
-//       color: var(--color-blue2)
-//     }
-//     `
-//   }
-//   return acc;
-// }
-
-// const grid = css`
-//   ${generateSpanStyles(startRange, endRange)}
-// `
-// const range = (max) => [...Array(max).keys()]
-
-// const setUpGen = val => range(endRange).map(val)
-
-
-// const grid =  css`
-//   .span-${step} {
-//     grid-column-end: span ${step}
-//   }
-// `
-
-
-
-// console.log(grid)
-// console.log(testing)
-
-
-//todo
-const collapseStyles = (breakpoint) => (i) => `
-  .span-0${breakpoint} {
-    display: none;
-  }
-
-  .row-span-0${breakpoint} {
-    display: none;
-  }
-`
+// This can later come from rhythm
+const spacing = {
+    0: "0",
+    8: "0.5rem",
+    16: "1rem",
+    32: "2rem",
+    64: "4rem"
+};
 
 const columnStyles = (breakpoint) => (i) => `
   .span-${i}${breakpoint} {
@@ -89,11 +45,11 @@ const startStyles = (breakpoint) => (i) => `
   }
 `
 
-
 const spanStyles = (breakpoint) => (i) => `
-  .row-span-${i}${breakpoint} {
-    -ms-grid-row-span: ${i};
-    grid-row: span ${i};
+  .span-${i}${breakpoint} {
+    -ms-grid-column-span: ${i};
+    grid-column-end: span ${i};
+    display: initial;
   }
 `
 
@@ -105,18 +61,77 @@ const rowStyles = (breakpoint) => (i) => `
 `
 
 
+// doesn't care about iterating 
+const collapseStyles = (breakpoint) => (i) => `
+  .span-0${breakpoint} {
+    display: none;
+  }
+
+  .row-span-0${breakpoint} {
+    display: none;
+  }
+`
+
+// gaps interate from gap object
+const gapStyles = (breakpoint) => (spacing) => `
+  .gap-${spacing}${breakpoint} {
+      grid-gap: ${spacing}
+  }
+`
+
+const gapColStyles = (breakpoint) => (spacing) => `
+  .gap-col-${spacing}${breakpoint} {
+      grid-column-gap: ${spacing}
+  }
+`
+
+const gapRowStyles = (breakpoint) => (spacing) => `
+  .gap-col-${spacing}${breakpoint} {
+      grid-row-gap: ${spacing}
+  }
+`
+
+// row for ms polyfill
+const msGridRows = (i) => `
+  .ms-row-${i} {
+      -ms-grid-row: ${i}
+  }
+`
+
+// not converted yet 
+const msGridGapStyles = () => `
+@media screen and (-ms-high-contrast: active), (-ms-high-contrast: none) {
+    /* creates a margin from 5 to 25 in multiples of 5 */
+    @each $pt, $ptSize in $spacing-points {
+      .gap-#{$pt}#{$val} {
+        margin-right: -#{$ptSize};
+        margin-bottom: -#{$ptSize};
+      }
+
+      .gap-#{$pt}#{$val} > * {
+        margin-right: #{$ptSize};
+        margin-bottom: #{$ptSize};
+      }
+    }
+  }
+`
+
+
+// doesn't care about breakpoints
+const gridStyles = (i) => `
+  .grid-${i} {
+    display: -ms-grid;
+    display: grid;
+    -ms-grid-columns: ${'1fr '.repeat(i)};
+    grid-template-columns: repeat(${i}, 1fr);
+  }
+`
+
 const getIterableStyles = (arr, breakpoint, style) => arr
     .map(style(breakpoint))
     .join('');
 
-const range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-// const breakpoints = ['', '-md', '-lg'];
-const breakpoints = {
-    '': '0px',
-    '-md': '768px',
-    '-lg': '992'
-}
 const generatedCss =
     Object.keys(breakpoints)
         .map(breakpoint => css`
@@ -125,6 +140,7 @@ const generatedCss =
     ${getIterableStyles(range, breakpoint, startStyles)}
     ${getIterableStyles(range, breakpoint, spanStyles)}
     ${getIterableStyles(range, breakpoint, rowStyles)}
+    ${getIterableStyles(range, breakpoint, gridStyles)}
 }
 `
         )
